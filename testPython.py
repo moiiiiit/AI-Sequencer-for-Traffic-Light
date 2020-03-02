63fb8493463c45573841af84d144dfbd432b9ed7
moiiiiit <43913328+moiiiiit@users.noreply.github.com>
2020-03-01 19:36:40 -0600

    meh

--- testPython.py
+++ testPython.py
@@ -1,6 +1,8 @@
+#I was confused on how Python instances work so I did this
+
 class boomer:
     def __init__(self, fruit):
-
+        self.fru = fruit
         self.frui = fruit.copy()
 
 
@@ -15,8 +17,14 @@ print(fruits)
 print(instance1.frui)
 print(instance2.frui)
 
+print(instance1.fru)
+print(instance2.fru)
+
 instance1.frui.pop(0)
 instance2.frui.pop(1)
 
 print(instance1.frui)
 print(instance2.frui)
+
+print(instance1.fru)
+print(instance2.fru)
